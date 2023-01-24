f3b22724a668c16e873a80690a9bd68825373d64
SAMUELDESK\samue <samuelsilvapb07@gmail.com>
2023-01-24 17:54:00 -0300

    Melhoria no codigo, mantem a acao do ultimo operador caso continue apertando a tecla Igual

--- Calculadora/scripts/controller/CalcController.js
+++ Calculadora/scripts/controller/CalcController.js
@@ -2,6 +2,9 @@ class CalcController {
 
     constructor(){
 
+        //Calcs
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         
         // Date and Time
@@ -90,15 +93,32 @@ class CalcController {
 
     };
 
+    getResult(){
+
+        return eval(this._operation.join(""));
+    };
+
     calc(){
 
         let last = '';
 
+        this._lastOperator = this.getLastItem(); 
+
+        if(this._operation.length < 3){
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
+
         if(this._operation.length > 3){
             last = this._operation.pop();
-        };       
+           
+            this._lastNumber = this.getResult();
+        } else if (this._operation.length == 3){
+                                 
+            this._lastNumber = this.getLastItem(false);
+        };
         
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
 
         if(last == '%'){
 
@@ -118,20 +138,30 @@ class CalcController {
 
     };
 
-    setLastNumberToDisplay(){
-         
-        let lastNumber;
+    getLastItem(isOperator = true){
+        let lastItem;
     
         for (let i = this._operation.length-1; i >= 0; i--){
-            // Searches for the last number
-            if(!this.isOperator(this._operation[i])){
-                // If it's not a operator set the variable
-
-                lastNumber = this._operation[i]
+            // Searches for the last Item
+            if(this.isOperator(this._operation[i]) == isOperator){
+                // If it is a operator set the variable
+                lastItem = this._operation[i]
                 break;
             };
-
+ 
         };
+
+        if(!lastItem){
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        };
+
+        return lastItem;
+
+    };
+
+    setLastNumberToDisplay(){
+         
+        let lastNumber = this.getLastItem(false);
 
         if (!lastNumber) lastNumber = 0;
 
